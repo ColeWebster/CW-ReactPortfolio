034616c76ee144597e931e50587ec2cad8e02a37
Cole Webster <Cole.Webster@gmail.com>
2022-03-02 15:50:33 -0500

    Updating portfolio with FreeCodeAcademy template to deconstruct and learn some items for later use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import Navbar from "./components/navbar.js";
+import About from "./components/about.js";
+import Projects from "./components/projects.js";
+import Contact from "./components/contact.js";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
   );
 }
 
-export default App;
