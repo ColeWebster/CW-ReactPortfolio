470280d0e9cb3c594fa5a992250fe780b7cbdde2
Cole Webster <Cole.Webster@gmail.com>
2022-03-02 22:36:14 -0500

    Updated nav and about me

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 
 import Navbar from "./components/navbar.js";
 import About from "./components/about.js";
-import Projects from "./components/projects.js";
-import Contact from "./components/contact.js";
+// import Projects from "./components/projects.js";
+// import Contact from "./components/contact.js";
+
+
 
 export default function App() {
   return (
       <main className="text-gray-400 bg-gray-900 body-font">
         <Navbar />
         <About />
-        <Projects />
-        <Contact />
+        {/* <Projects />
+        <Contact /> */}
       </main>
   );
 }
--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import photo from "../images/profile.jpg"
  
 export default function about(props) {
   return (
@@ -6,34 +7,20 @@ export default function about(props) {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi! I'm Cole.
+            <br className="hidden lg:inline-block" />Thanks for visiting my portfolio
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+           Currently I am finishing a full stack bootcamp at the University of Central Florida. During this time I have grown and added many skills to my resume. I love using react to make pages look amazing and would be happy to connect with other developers to grow and learn. 
           </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
-            </a>
-          </div>
+          <p className="mb-8 leading-relaxed">
+          Prior to learning web development, I spent eight years at Apple as a technical support advisor across all products. My greatest accomplisment was helping launch @AppleSupport and 
+          </p>
+          
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
+         
+        <div className="flex flex-wrap justify-center">
+          <img src={photo} class="shadow-lg rounded-full max-w-md h-auto align-middle border-none" />
         </div>
       </div>
     </section>
